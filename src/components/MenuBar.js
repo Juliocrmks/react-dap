c7746acf1e13d87141ddf88b077c997c1ada36f7
Julio Ruiz <julio.cr.mks@gmail.com>
2022-11-28 22:36:40 -0600

    second

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,21 +1,77 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { Link } from 'react-router-dom';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+// import { mainListItems, secondaryListItems } from './listItems';
+
+const drawerWidth = 240;
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
 export default function MenuBar() {
   const [auth, setAuth] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   const handleChange = (event) => {
     setAuth(event.target.checked);
@@ -30,7 +86,7 @@ export default function MenuBar() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <>
       {/* <FormGroup>
         <FormControlLabel
           control={
@@ -43,54 +99,61 @@ export default function MenuBar() {
           label={auth ? 'Logout' : 'Login'}
         />
       </FormGroup> */}
-      <AppBar position="static">
-        <Toolbar>
+
+      <AppBar position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: '24px', // keep right padding when drawer closed
+          }}
+        >
           <IconButton
-            size="large"
             edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' }),
+            }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Halcon
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Dashboard
           </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem as={Link} to={'/about'} onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
         </Toolbar>
       </AppBar>
-    </Box>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          {/* {mainListItems} */}
+          <Divider sx={{ my: 1 }} />
+          {/* {secondaryListItems} */}
+        </List>
+      </Drawer>
+      </>
   );
 }
--- src/pages/CustomerDashboard.js
+++ src/pages/CustomerDashboard.js
@@ -18,6 +18,33 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { DataGrid } from '@mui/x-data-grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { TablePagination } from "@mui/material";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 // import { mainListItems, secondaryListItems } from './listItems';
 
 
@@ -25,95 +52,52 @@ import { DataGrid } from '@mui/x-data-grid';
 function CustomerDashboard() {
 
   const columns = [
-    { field: 'id', headerName: 'Invoice ID', width: 70 },
-    { field: 'customerid', headerName: 'Customer ID', width: 130 },
-    { field: 'date', headerName: 'Date', width: 130 },
+    { id: 'id', headerName: 'Invoice ID', width: 70 },
+    { id: 'customerid', headerName: 'Customer ID', width: 130 },
+    { id: 'date', headerName: 'Date', width: 130 },
     {
-      field: 'status',
+      id: 'status',
       headerName: 'Status',
     },
     {
-      field: 'action',
+      id: 'action',
       headerName: 'action',
       description: 'This column has a value getter and is not sortable.',
       sortable: false,
       width: 160,
     },
   ];
-  
+
   const rows = [
-    { id: 1, customerid: 'Snow', date: 'Jon', status: 'active', action:'action' },
-    { id: 2, customerid: 'Lannister', date: 'Cersei', status: 'active', action:'action' },
-    { id: 3, customerid: 'Lannister', date: 'Jaime', status: 'active', action:'action' },
-    { id: 4, customerid: 'Stark', date: 'Arya', status: 'active', action:'action' },
-    { id: 5, customerid: 'Targaryen', date: 'Daenerys', status: 'active', action:'action' },
-    { id: 6, customerid: 'Melisandre', date: null, status: 'active', action:'action' },
-    { id: 7, customerid: 'Clifford', date: 'Ferrara', status: 'active', action:'action' },
-    { id: 8, customerid: 'Frances', date: 'Rossini', status: 'active', action:'action' },
-    { id: 9, customerid: 'Roxie', date: 'Harvey', status: 'active', action:'action' },
+    { id: 1, customerid: 'Snow', date: 'Jon', status: 'active', action: 'action' },
+    { id: 2, customerid: 'Lannister', date: 'Cersei', status: 'active', action: 'action' },
+    { id: 3, customerid: 'Lannister', date: 'Jaime', status: 'active', action: 'action' },
+    { id: 4, customerid: 'Stark', date: 'Arya', status: 'active', action: 'action' },
+    { id: 5, customerid: 'Targaryen', date: 'Daenerys', status: 'active', action: 'action' },
+    { id: 6, customerid: 'Melisandre', date: null, status: 'active', action: 'action' },
+    { id: 7, customerid: 'Clifford', date: 'Ferrara', status: 'active', action: 'action' },
+    { id: 8, customerid: 'Frances', date: 'Rossini', status: 'active', action: 'action' },
+    { id: 9, customerid: 'Roxie', date: 'Harvey', status: 'active', action: 'action' },
   ];
   const [open, setOpen] = React.useState(true);
   const toggleDrawer = () => {
     setOpen(!open);
   };
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      {/* <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar> */}
-      {/* <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer> */}
       <Box
         component="main"
         sx={{
@@ -136,21 +120,65 @@ function CustomerDashboard() {
                   p: 2,
                   display: 'flex',
                   flexDirection: 'column',
-                  height:400,
+                  height: 400,
                 }}
-              >
+              ><TableContainer component={Paper}>
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map((column) => (
+                          <TableCell
+                            key={column.field}
+                          >
+                            {column.headerName}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => {
+                          return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                              {columns.map((column) => {
+                                const value = row[column.id];
+                                return (
+                                  <TableCell key={column.id} >
+                                    {value}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 100]}
+                  component="div"
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+
                 {/* <Chart /> */}
-                <DataGrid
+                {/* <DataGrid
                   rows={rows}
                   columns={columns}
                   pageSize={5}
                   rowsPerPageOptions={[5]}
                   checkboxSelection
-                />
+                /> */}
+
+
               </Paper>
             </Grid>
             {/* Recent Deposits */}
-            
+
           </Grid>
         </Container>
       </Box>
